548b36bab295891b1f9a5d195dc36e6c93208104
Sergei Degtiarev <sdegtiarev@yahoo.com>
2016-10-18 14:10:32 -0400

    shared_from_this variant

---
+++ t4.cc
@@ -0,0 +1,26 @@
+// unsafe version, dumps core
+// using move ctor
+#include <thread>
+#include <memory>
+#include <iostream>
+
+struct seed : std::enable_shared_from_this<seed>
+{};
+
+auto map=std::make_shared<seed>();
+
+void inc() {
+	static int cnt=0, last=0;
+	for(;; ++cnt) try {
+		auto sp=map->shared_from_this();
+	} catch(...) { std::cerr<<cnt-last<<std::endl; last=cnt; }
+}
+
+int main()
+{
+	std::thread(inc).detach();
+	for(;;)
+		map=std::make_shared<seed>();
+	return 0;
+}
+
